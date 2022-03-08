6319954b01d04c3df11b963e742440cfba40740c
nothub <48992448+nothub@users.noreply.github.com>
2022-03-08 16:04:59 +0100

    update util script

--- notes.sh
+++ notes.sh
@@ -1,21 +1,58 @@
 #!/usr/bin/env bash
 
-# TODO: arg parsing
-# TODO: verbose flag
-# TODO: create new notes subdir with command
 # TODO: build only specified notes
-# TODO: update eisvogel with command
 
-set -Eeu
-set -x
-set -o pipefail
+set -Eeuo pipefail
 
 dir_root=$(dirname "$(readlink -f "$0")")
 dir_notes=$dir_root/notes
 dir_out=$dir_root/out
 
 log() {
-    echo >&2 "$1"
+    echo >&2 "${1-}"
+}
+
+init() {
+    if [ -d "$dir_notes"/"$1" ]; then
+        log "directory present: $(realpath "$1")"
+        exit 1
+    fi
+
+    log "creating notes directory: $(realpath "$1")"
+    mkdir -p "$dir_notes"/"$1"
+
+    cat <<EOF >"$dir_notes"/"$1"/metadata.yaml
+---
+title: "To foo a bar"
+date: "1 January 1970"
+---
+EOF
+
+    cat <<EOF >"$dir_notes"/"$1"/00_notes.md
+# Heading Level 1
+
+Example Text
+
+## Heading Level 2
+
+Example figure
+
+![Coffee](https://www.w3.org/Icons/Misc/coffee.gif)
+EOF
+
+    exit 0
+}
+
+usage() {
+    cat <<EOM
+Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] -n name arg1 [arg2...]
+
+Available options:
+-h    Print this help and exit
+-v    Enable verbose output
+-n    Initialize new notes directory
+EOM
+    exit
 }
 
 build() {
@@ -51,6 +88,16 @@ build() {
         --output="$dir_out"/"$1".pdf
 }
 
+mkdir -p "$dir_notes"
+
+while getopts vn:h opt; do
+    case $opt in
+    v) set -x ;;
+    n) init "$OPTARG" ;;
+    h | *) usage ;;
+    esac
+done
+
 if ! command -v pandoc &>/dev/null; then
     log "pandoc not found!"
     exit 1
@@ -59,9 +106,7 @@ fi
 rm -rf "$dir_out"
 mkdir -p "$dir_out"
 
-mkdir -p "$dir_notes"
 cd "$dir_notes"
-
 for dir in *; do
     build "$dir"
 done
