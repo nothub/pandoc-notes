f3c13f59e31d1d6e0d56453f7e75eac60af975b4
nothub <48992448+nothub@users.noreply.github.com>
2022-03-08 14:50:21 +0100

    update build script

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
 # TODO: arg parsing
+# TODO: verbose flag
 # TODO: create new notes subdir with command
 # TODO: build only specified notes
 # TODO: update eisvogel with command
 
-set -Eeuxo pipefail
+set -Eeu
+set -x
+set -o pipefail
 
 dir_root=$(dirname "$(readlink -f "$0")")
 dir_notes=$dir_root/notes
@@ -13,6 +16,39 @@ dir_out=$dir_root/out
 
 log() {
     echo >&2 "$1"
+}
+
+build() {
+    echo building "$(realpath "$1")"
+
+    if [ ! -d "$1" ]; then
+        log "not a directory: $(realpath "$1")"
+        return
+    fi
+
+    if [ ! -f "$1"/metadata.yaml ]; then
+        log "missing metadata.yaml in: $(realpath "$1")"
+        return
+    fi
+
+    if [ ! -f "$1"/notes.md ]; then
+        log "missing notes.md in: $(realpath "$1")"
+        return
+    fi
+
+    pandoc --standalone \
+        --fail-if-warnings \
+        --highlight-style tango \
+        --number-sections \
+        --table-of-contents \
+        --toc-depth=2 \
+        --from=markdown \
+        --to=latex \
+        --template=../common/eisvogel.tex \
+        "$1"/metadata.yaml \
+        "$1"/notes.md \
+        ../common/metadata.yaml \
+        --output="$dir_out"/"$1".pdf
 }
 
 if ! command -v pandoc &>/dev/null; then
@@ -26,32 +62,6 @@ mkdir -p "$dir_out"
 mkdir -p "$dir_notes"
 cd "$dir_notes"
 
-for this in *; do
-
-    echo building "$this"
-
-    if [ ! -f "$this"/metadata.yaml ]; then
-        log "missing metadata.yaml in $this"
-        continue
-    fi
-
-    if [ ! -f "$this"/notes.md ]; then
-        log "missing notes.md in $this"
-        continue
-    fi
-
-    pandoc --standalone \
-        --fail-if-warnings \
-        --highlight-style tango \
-        --number-sections \
-        --table-of-contents \
-        --toc-depth=2 \
-        --from=markdown \
-        --to=latex \
-        --template=../common/eisvogel.tex \
-        "$this"/metadata.yaml \
-        "$this"/notes.md \
-        ../common/metadata.yaml \
-        --output="$dir_out"/"$this".pdf
-
+for dir in *; do
+    build "$dir"
 done
